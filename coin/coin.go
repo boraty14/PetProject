df783f332c9ad1a81a58f84116469fb15f9ba9fd
braty <brataysen@gmail.com>
2024-02-01 19:42:53 +0300

    particle added

--- coin/coin.go
+++ coin/coin.go
@@ -13,6 +13,21 @@ components {
     w: 1.0
   }
 }
+components {
+  id: "pick_up"
+  component: "/coin/pick_up.particlefx"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
